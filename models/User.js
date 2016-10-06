8b6a57ba22c6d6aa8c76246da923e12aa8665254
Aziz <azizsaboor@gmail.com>
2016-10-06 14:42:22 +0800

    added user model, with schema validation

---
+++ models/User.js
@@ -0,0 +1,42 @@
+/**
+ * User.js 
+ * - User Schema and Model Definition
+ * - please note the mongoose schema validators in use
+ */
+
+
+var mongoose = require('mongoose'),
+    UserSchema = new mongoose.Schema({
+        email: {
+            type: String,
+            lowercase: true,
+            unique: true,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        profile: {
+            firstName: {
+                type: String
+            },
+            lastName: {
+                type: String
+            }
+        },
+        role: {
+            type: String,
+            enum: ['Member', 'Client', 'Owner', 'Admin'],
+            default: 'Member'
+        },
+        resetPasswordToken: {
+            type: String
+        },
+        resetPasswordExpress: {
+            type: Date
+        }
+    },
+    {
+        timestamps: true
+    })
