68a547242e6b27e7165b90693ba883b7e3b31a48
Aziz <azizsaboor@gmail.com>
2016-10-07 11:12:18 +0800

    improved comment and section structure

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-/**
+/*
  * config.js
  * - contains all the sensitive global configurations
  * - should normally NOT be included in git
--- index.js
+++ index.js
@@ -1,36 +1,42 @@
-/**
+/*
  * index.js 
  * - the main entry point for this node server app
+ * - please note that CSRF attacks are a danger for session or cookie based applications
+ * - however this application makes use of a session-less technology called JWT along with localStorage
  */
 
 
-// settle dependencies
-var express = require('express'),
+
+/* 
+ * settle requirements 
+ */
+
+const express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     logger = require('morgan'),
     mongoose = require('mongoose'),
-    bcrypt = require('bcrypt-nodejs'),
-    config = require('./config.js'),
-    User = require('./models/User');
+
+    // sensitive configurations
+    config = require('./config.js');
 
 
-// setup middleware
 
-// 'extended: false' uses querystring library
-// but 'extended: true' uses qs library thats parses objects or any other types, 
-// allowing for JSON-like experience 
+/* 
+ * setup middleware i.e. connect database, set logger, etc 
+ */
+
+mongoose.connect(config.database);
+
+// 'false' uses querystring library for shallow parsing, 
+// and 'true' uses qs library for deep parsing
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // parse application/json
 app.use(logger('dev'));
 
-// please note that CSRF attacks are a danger for session or cookie based applications
-// however this application makes use of a session-less technology called JWT along with localStorage
-
-// enable CORS from client-side
-// please note that settings ACAO to '*' will normally prevent requests to supply credentials,
+// enable CORS from client-side 
+// setting to '*' prevents requests to supply credentials 
 // i.e. (HTTP authentication, client-side SSL certificates, cookies)
-// however, however it will permit us to provide a token within the HTTP header
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -38,9 +44,12 @@ app.use(function (req, res, next) {
 });
 
 app.set('port', process.env.PORT || 3000);
-mongoose.connect(config.database);
 
 
-// start the server
-var server = app.listen(app.get('port'));
+
+/* 
+ * start the server 
+ */
+
+app.listen(app.get('port'));
 console.log('Your server is running on port ' + app.get('port') + '.');
