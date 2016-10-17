c36104bf1dbcd3ef252cd00f3c6263ba9f51e72a
Aziz Saboor <azizsaboor@gmail.com>
2016-10-17 18:33:44 +0800

    defined /api/auth routes

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,38 @@
 
 
 
-var express = require('express'),
-    passport = require('./../config/passport'),
-    authentication = require('./../controllers/authentication'),
+/*
+ * setup dependencies
+ */
 
-    // route middleware, ensure relevant authentication upon certain route requests 
-    // without the use of sessions
-    requireAuth = passport.authenticate('jwt', { session: false }),
-    requireLogin = passport.authenticate('local', { session: false });
+var express = require('express'),
+    routerAuth = express.Router(),
+    routerAPI = express.Router(),
+    passport = require('./../config/passport'),
+    Authentication = require('./../controllers/authentication'),
+
+    // define passport route middleware, plugged into route to ensure usage
+    passportLogin = passport.authenticate('local', { session: false }),
+    passportJwt = passport.authenticate('jwt', { session: false });
+
+
+
+/*
+ * setup routes
+ */
+
+// simply call the signup method, no need for 'passportJwt' to extract jwt from header
+routerAuth.post('/signup', Authentication.signup);
+
+// call the passport login strategy first, and then the signin method
+routerAuth.post('/signin', passportLogin, Authentication.signin);
+
+// for protected routes, passport to extract jwt from header, and verify it first 
+// router.get('/protected', passportJwt, FooController.doSomething);
+
+// bind above routes to become subroutes of '/auth'
+routerAPI.use('/auth', routerAuth);
+
+
+
+module.exports = routerAPI;
