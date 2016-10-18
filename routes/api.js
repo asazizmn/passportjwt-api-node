32b8c7319f3398d37ab5e528875218a7cf9ecaff
Aziz Saboor <azizsaboor@gmail.com>
2016-10-18 18:32:33 +0800

    added testing route for random protected data

--- routes/api.js
+++ routes/api.js
@@ -12,6 +12,7 @@
 var express = require('express'),
     routerAuth = express.Router(),
     routerAPI = express.Router(),
+    routerTest = express.Router(), // REMOVE before production
     passport = require('./../config/passport'),
     Authentication = require('./../controllers/authentication'),
 
@@ -25,17 +26,27 @@ var express = require('express'),
  * setup routes
  */
 
+// for '/api/auth'
+
 // simply call the signup method, no need for 'passportJwt' to extract jwt from header
 routerAuth.post('/signup', Authentication.signup);
 
 // call the passport login strategy first, and then the signin method
 routerAuth.post('/signin', passportLogin, Authentication.signin);
 
+// bind above routes to become subroutes of '/auth'
+routerAPI.use('/auth', routerAuth);
+
+
+// for testing - REMOVE before production - '/api'
+
 // for protected routes, passport to extract jwt from header, and verify it first 
 // router.get('/protected', passportJwt, FooController.doSomething);
 
-// bind above routes to become subroutes of '/auth'
-routerAPI.use('/auth', routerAuth);
+// REMOVE
+routerAPI.get('/protected', passportJwt, function(req, res) {  
+    res.json({success: true, message: 'It worked! User id is: ' + req.user._id + '.'});
+});
 
 
 
